da6374f4ed68516933e4e5466ebc8fa2c043ea37
Geoffrey Tran <geoffrey.tran@gmail.com>
2014-05-30 16:29:37 -0500

    Update DblibPlatform.php

--- Platforms/DblibPlatform.php
+++ Platforms/DblibPlatform.php
@@ -134,6 +134,7 @@ class DblibPlatform extends SQLServerPlatform
 
         // add uniqueidentifier
         $this->doctrineTypeMapping['uniqueidentifier'] = 'uniqueidentifier';
+        $this->doctrineTypeMapping['date']             = 'date';
     }
 
     /**
